5f70449bdc37840e387b23024250029c4753d367
00Mugen00 <genericblue36@gmail.com>
2019-01-04 00:01:15 +0100

    NumberWizard script added

---
+++ NumberWizardGame/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField]
+    int max;
+    [SerializeField]
+    int min;
+    [SerializeField]
+    TextMeshProUGUI guessText;
+
+    int guess;
+
+    void Start()
+    {
+        NextGuess();
+        max = max + 1;
+    }
+
+
+    public void OnPressHigher()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        guessText.text = "Are you thinking about " + guess.ToString() + "?";
+    }
+}
